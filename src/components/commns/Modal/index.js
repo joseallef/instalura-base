c827419986e1abaa9048bc234bc77d07b11938b6
joseallef <46696111+joseallef@users.noreply.github.com>
2021-03-01 08:14:15 -0300

    Adicionando ajustes no modal aula 02 do modulo 02

--- src/components/commns/Modal/index.js
+++ src/components/commns/Modal/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import style, { css } from 'styled-components';
+import { motion } from 'framer-motion';
 
 const ModalWrapper = style.div`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  background: rgba(0,0,0,0.1);
+  background: rgba(0,0,0,0.9);
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   margin: auto;
-  overflow: scroll;
+  overflow: hidden;
+  transition: .3s;
+  z-index: 100;
+
 
 
   ${({ isOpen }) => {
@@ -46,9 +50,29 @@ function Modal({ isOpen, onClose, children }) {
         }
       }}
       >
-      {children({
-        'data-modal-safe-area': 'true',
-      })}
+        <motion.div
+          variants={{
+            open: {
+              x: 0,
+            },
+            closed: {
+              x: '-100%',
+            },
+          }}
+
+          animate={isOpen ? 'open' : 'closed'}
+          transition={{
+            duration: 0.5,
+          }}
+          style={{
+            display: 'flex',
+            flex: 1,
+          }}
+        >
+          {children({
+            'data-modal-safe-area': 'true',
+          })}
+        </motion.div>
     </ModalWrapper>
   )
 }
