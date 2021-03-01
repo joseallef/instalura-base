15e0eb40ddeeacdbdcb7f5d2b491d33127ef0f89
joseallef <46696111+joseallef@users.noreply.github.com>
2021-03-01 18:11:12 -0300

    Correção de erro

--- script-test.js
+++ script-test.js
@@ -1,8 +1,8 @@
-// eslint-disable-next-line no-undef
+// eslint-disable-next-line import/no-extraneous-dependencies
 const shell = require('shelljs');
 
 console.log('Ola mundo');
 
-const resultado = shell.exec('git diff --name-only branch-script-testes..main', { silent: true })
+const resultado = shell.exec('git diff --name-only branch-scripts-testes..main', { silent: true })
 
 console.log(resultado);
